698bc5513431e09000d881fd25e03591a6c6bc6f
Jonatan Shlomov <jonatanshv@gmail.com>
2024-12-19 23:55:11 +0200

    converting and validating all the data and inserting to mongodb

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from app.db.mongo_database import db_url, db_name, collection_name
-from app.db.repository.attack_repository import AttackRepository
-from app.db.repository.csv_repository import load_csv_to_mongo
+from app.servise.attack_service import process_and_insert_data
 
 if __name__ == "__main__":
+    first_csv_path = 'db/data/globalterrorismdb_0718dist.csv'
+    second_csv_path = 'db/data/RAND_Database_of_Worldwide_Terrorism_Incidents.csv'
 
-    file_path = "db/data/terror attacks 1000 rows.csv"
-
-    repository = AttackRepository(db_url, db_name, collection_name)
-    result = load_csv_to_mongo(file_path, repository)
-    print(f"Inserted {len(result)} records into MongoDB.")
+    process_and_insert_data(
+        first_csv_path,
+        second_csv_path,
+        db_url,
+        db_name,
+        collection_name
+    )
---
+++ app/servise/attack_service.py
@@ -0,0 +1,68 @@
+from typing import Optional
+import pandas as pd
+from app.db.models.attack import AttackModel
+from app.db.models.location import LocationModel
+from app.db.repository.attack_repository import AttackRepository
+from app.db.repository.csv_repository import create_df_first_csv, create_df_second_csv
+
+
+def validate_and_transform(row) -> Optional[AttackModel]:
+    try:
+        location = LocationModel(
+            city=str(row.get("city")) if pd.notna(row.get("city")) else None,
+            country=str(row.get("country_txt")) if pd.notna(row.get("country_txt")) else None,
+            region=str(row.get("region_txt")) if pd.notna(row.get("region_txt")) else None,
+            lat=row.get("latitude") if pd.notna(row.get("latitude")) else None,
+            lon=row.get("longitude") if pd.notna(row.get("longitude")) else None,
+            location=str(row.get("location")) if pd.notna(row.get("location")) else None
+        )
+
+        attack_types = [
+            str(row.get("attacktype1_txt")) if pd.notna(row.get("attacktype1_txt")) else None,
+            str(row.get("attacktype2_txt")) if pd.notna(row.get("attacktype2_txt")) else None,
+            str(row.get("attacktype3_txt")) if pd.notna(row.get("attacktype3_txt")) else None
+        ]
+        attack_types = [atype for atype in attack_types if atype]
+
+        target_types = [
+            str(row.get("targtype1_txt")) if pd.notna(row.get("targtype1_txt")) else None,
+            str(row.get("targtype2_txt")) if pd.notna(row.get("targtype2_txt")) else None,
+            str(row.get("targtype3_txt")) if pd.notna(row.get("targtype3_txt")) else None
+        ]
+        target_types = [ttype for ttype in target_types if ttype]
+
+        group_names = [
+            str(row.get("gname")) if pd.notna(row.get("gname")) else None,
+            str(row.get("gname2")) if pd.notna(row.get("gname2")) else None,
+            str(row.get("gname3")) if pd.notna(row.get("gname3")) else None
+        ]
+        group_names = [gname for gname in group_names if gname]  # Remove None values
+
+        return AttackModel(
+            event_id=str(row.get("eventid")),
+            date=row.get("date").strftime("%Y-%m-%d") if pd.notna(row.get("date")) else None,
+            location=location,
+            attack_types=attack_types,
+            target_types=target_types,
+            group_names=group_names,
+            fatalities=int(row.get("nkill")) if pd.notna(row.get("nkill")) else None,
+            injuries=int(row.get("nwound")) if pd.notna(row.get("nwound")) else None,
+            terrorists_num=int(row.get("nperps")) if pd.notna(row.get("nperps")) else None,
+            summary=str(row.get("summary")) if pd.notna(row.get("summary")) else None
+        )
+    except ValueError as e:
+        print(f"Validation error for row {row.get('eventid')}: {e}")
+        return None
+
+def process_and_insert_data(first_csv_path, second_csv_path, db_url, db_name, collection_name):
+    first_df = create_df_first_csv(first_csv_path)
+    second_df = create_df_second_csv(second_csv_path)
+    combined_df = pd.concat([first_df, second_df], ignore_index=True)
+    combined_df = combined_df.drop_duplicates(
+        subset=['date','gname','nkill','nwound','city','country_txt', 'summary'],
+        keep='first'
+    )
+    repository = AttackRepository(db_url, db_name, collection_name)
+
+    attacks = [validate_and_transform(row) for _, row in combined_df.iterrows()]
+    repository.insert_many_attacks(attacks)
